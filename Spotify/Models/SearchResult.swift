71aba4bfbbb561987af8b118d8c749633be456fe
Anton Omelchuk <kino_kg@ukr.net>
2021-10-24 21:56:37 +0300

    search results ui

---
+++ Spotify/Models/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResult.swift
+//  Spotify
+//
+//  Created by Anton Omelchuk on 24.10.2021.
+//
+
+import Foundation
---
+++ Spotify/Models/SearchResultResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultResponse.swift
+//  Spotify
+//
+//  Created by Anton Omelchuk on 24.10.2021.
+//
+
+import Foundation
